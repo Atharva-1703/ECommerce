fe8e4685e53cb98e344391d22c2258ac67ab20ee
Atharva-1703 <141930344+Atharva-1703@users.noreply.github.com>
2025-07-06 17:04:36 +0530

    added cart reference to model

--- backend/models/Orders.js
+++ backend/models/Orders.js
@@ -1,5 +1,31 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-const orderSchema={
-    
-}
+const orderSchema = {
+  name: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
+  quantity: { type: Number, required: true,min:1 },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  shippingAddress: {
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    postalCode: { type: String, required: true },
+    country: { type: String, required: true },
+  },
+};
+
+const Orders= mongoose.model("Orders", orderSchema);
+module.exports = Orders;
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,29 +1,45 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema={
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    favourites: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
       },
-      email: {
-        type: String,
-        required: true,
-        unique: true,
+    ],
+    cart: [
+      {
+        products: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
       },
-      password: {
-        type: String,
-        required: true,
-      },
-      isAdmin: {
-        type: Boolean,
-        required: true,
-        default: false,
-      },
-      favourites:[
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"Product"
-        }
-      ],
-      
-}
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
