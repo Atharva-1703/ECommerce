2e23876d58ebdf5894cf79bc47e5a41991fed1f8
Atharva-1703 <141930344+Atharva-1703@users.noreply.github.com>
2025-08-04 20:41:22 +0530

    rearranging components based on their use in more than one component or page or a single page somponent

--- frontend/src/components/Home/Discounts/Discounts.tsx
+++ frontend/src/components/Home/Discounts/Discounts.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { Product } from "@/types";
-import ProductCard from "../ProductCard/ProductCard";
+import ProductCard from "../../Common/ProductCard/ProductCard";
 
 export default function Discounts({ products }: { products: Product[] }) {
   return (
-    <section className="relative py-8 bg-white">
+    <section className="relative py-8 ">
       <h2 className="text-2xl font-bold mb-4 px-4">Huge Discounts</h2>
       <div
         className="px-2 pb-1 md:px-4 flex gap-3 overflow-x-scroll scrollbar-custom"
