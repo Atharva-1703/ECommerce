613f92c017ec063a72ef63c2f712cbd1ed952f74
Atharva-1703 <141930344+Atharva-1703@users.noreply.github.com>
2025-09-04 17:20:44 +0530

    search store product filter functionality implemented

--- frontend/src/stores/useSearchStore.ts
+++ frontend/src/stores/useSearchStore.ts
@@ -6,65 +6,97 @@ interface Filters {
   category?: string;
   title?: string;
   sortBy?: string;
+  brands?:string[];
   order: "asc" | "desc";
   minPrice?: number;
   maxPrice?: number;
 }
 
+interface ProductFilter {
+  brands: string[];
+  categories?: string[];
+}
+
 interface SearchState {
   products: Product[];
-  brands: string[];
   filters: Filters;
   offset: number;
+  productFilter: ProductFilter;
   loading: boolean;
+  totalProducts:number;
   // setBrands: (brands: string[]) => void;
-  fetchProducts: () => Promise<void>;
+  fetchProducts: (passedfilters?:Filters) => Promise<void>;
   setfilters: (updates: Partial<Filters>) => void;
   incrementOffset: () => void;
   resetFilters: () => void;
-  fetchBrands: () => Promise<void>;
+  resetProductFilter: () => void;
+  resetOffset: () => void;
+  fetchProductFilters: (title?:string,category?:string) => Promise<void>;
 }
 
 export const useSearchStore = create<SearchState>((set, get) => ({
   products: [],
-  brands: [],
   offset: 0,
+  totalProducts:0,
   filters: {
     order: "asc",
+  },
+  productFilter: {
+    brands: [],
+    categories: [],
   },
   loading: false,
   incrementOffset: () => {
     set({ offset: get().offset + 10 });
   },
+  resetOffset: () => {
+    set({ offset: 0 });
+  },
   setfilters: (updates: Partial<Filters>) => {
     set({ filters: { ...get().filters, ...updates } });
   },
   resetFilters: () => {
-    set({ filters: {  order: "asc" } });
+    set({ filters: { order: "asc" } });
   },
-  fetchProducts: async () => {
+  resetProductFilter: () => {
+    set({ productFilter: { brands: [], categories: [] } });
+  },
+  fetchProducts: async (passedfilters) => {
     set({ loading: true });
-    const { products, filters,offset ,incrementOffset} = get();
+    const { products,  offset, incrementOffset } = get();
 
     const params = new URLSearchParams();
 
-    Object.entries(filters).forEach(([key, value]) => {
-      if (value) {
+    Object.entries(passedfilters||get().filters).forEach(([key, value]) => {
+      if(Array.isArray(value)) {
+        value.forEach((val) => params.append(key, val));
+      }
+      else if (value) {
         params.set(key, value.toString());
       }
     });
 
     const response = await fetch(
-      `${API_URL}/api/products?${params.toString()}`
+      `${API_URL}/api/products?${params.toString()}&offset=${offset}`
     );
     const data = await response.json();
     set({
-      products:
-        offset > 0 ? [...products, ...data.products] : data.products,
+      products: offset > 0 ? [...products, ...data.products] : data.products,
       loading: false,
     });
     incrementOffset();
-    
   },
-  fetchBrands: async () => {},
+  fetchProductFilters: async (title,category) => {
+    const { filters } = get();
+    const response = await fetch(
+      `${API_URL}/api/products/filters?category=${category||filters.category}&title=${title||filters.title}`
+    );
+    const data = await response.json();
+    if(data.filters.categories.length==0) {
+      data.filters.categories=[category]
+    }
+    set({ productFilter: data.filters });
+    // set({productFilter})
+    set({ totalProducts:data.totalProducts });
+  },
 }));
