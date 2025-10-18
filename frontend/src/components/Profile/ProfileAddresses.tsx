aa41f5d087b979764243bf6514eaf722f3ff025f
Atharva-1703 <141930344+Atharva-1703@users.noreply.github.com>
2025-10-18 17:56:54 +0530

    unnecessary import removed

--- frontend/src/components/Profile/ProfileAddresses.tsx
+++ frontend/src/components/Profile/ProfileAddresses.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { Icon } from "@iconify/react";
 import AddAddressForm from "./AddAddressForm";
 import { useUserStore } from "@/stores/useUserStore";
 import AddressCard from "./AddressCard";
