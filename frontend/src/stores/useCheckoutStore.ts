d18f853d1f61616657b17f379ab2c076b2880428
Atharva-1703 <141930344+Atharva-1703@users.noreply.github.com>
2025-10-26 17:47:17 +0530

    added and implemented place order and removed unnecessary variables and methods

--- frontend/src/stores/useCheckoutStore.ts
+++ frontend/src/stores/useCheckoutStore.ts
@@ -1,32 +1,51 @@
 import { Product } from "@/types";
+import { fetcher } from "@/utils/fetcher";
+import { API_URL } from "@/utils/url";
+import toast from "react-hot-toast";
 import { create } from "zustand";
 
 interface CheckoutState {
-    CheckoutItems: { product: Product, quantity: number }[],
-    addressId:string,
-    totalCost: number,
-    isCompleted: boolean,
-    
-    setCheckoutItems: (items: { product: Product, quantity: number }[]) => void
+  CheckoutItems: { product: Product; quantity: number }[];
+  addressId: string;
+  totalCost: number;
 
-    setTotalCost: (cost: number) => void
+  setCheckoutItems: (items: { product: Product; quantity: number }[]) => void;
 
-    setAddressId: (id: string) => void
+  setTotalCost: (cost: number) => void;
 
-    setIsCompleted: (isCompleted: boolean) => void
+  setAddressId: (id: string) => void;
 
+  placeOrder: (paymentMethod:"cod"|"upi"|"card") => Promise<void>;
 }
 
+const useCheckoutStore = create<CheckoutState>((set, get) => ({
+  CheckoutItems: [],
+  totalCost: 0,
+  addressId: "",
 
-const useCheckoutStore=create<CheckoutState>((set, get) => ({
-    CheckoutItems: [],
-    totalCost: 0,
-    addressId: "",
-    isCompleted: false,
-    setCheckoutItems: (items) => set({ CheckoutItems: items }),
-    setTotalCost: (cost) => set({ totalCost: cost }),
-    setAddressId: (id) => set({ addressId: id }),
-    setIsCompleted: (isCompleted) => set({ isCompleted }),
-}))
+  setCheckoutItems: (items) => set({ CheckoutItems: items }),
+  setTotalCost: (cost) => set({ totalCost: cost }),
+  setAddressId: (id) => set({ addressId: id }),
+  placeOrder: async (paymentMethod) => {
+    const { CheckoutItems, addressId} = get();
+    const items = CheckoutItems.map(({ product, quantity }) => ({
+      productId: product._id,
+      quantity,
+    }));
+    const toastId = toast.loading("Placing order...");
+    const res = await fetcher(`${API_URL}/api/orders/add`, "POST", {
+      items,
+      addressId,
+      paymentMethod
+    });
+    const data = await res.json();
+    if (!data.success) {
+      toast.error(data.message, { id: toastId });
+      return;
+    }
+    toast.success("Order placed successfully", { id: toastId });
+  },
 
-export default useCheckoutStore
+}));
+
+export default useCheckoutStore;
