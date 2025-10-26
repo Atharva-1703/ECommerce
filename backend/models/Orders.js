b67883e4e0d99bf20af8af707a64a0c044cc2b45
Atharva-1703 <141930344+Atharva-1703@users.noreply.github.com>
2025-10-26 16:56:20 +0530

    method name change

--- backend/models/Orders.js
+++ backend/models/Orders.js
@@ -32,7 +32,7 @@ const orderSchema = new mongoose.Schema(
 
     paymentMethod: {
       type: String,
-      enum: ["card", "cod", "paypal"],
+      enum: ["card", "cod", "upi"],
       default: "cod",
     }, 
 
