a5dc3614a5deb02b0ed9d4121641c4f1e307fb79
Atharva-1703 <141930344+Atharva-1703@users.noreply.github.com>
2025-11-27 15:32:16 +0530

    feat(searchbar): implemented a basic searchbar with results display

--- frontend/src/components/Common/Header/Header.tsx
+++ frontend/src/components/Common/Header/Header.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { Icon } from "@iconify/react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useUserStore } from "@/stores/useUserStore";
+import { useSearchStore } from "@/stores/useSearchStore";
 
 interface IconItem {
   key: "favourite" | "cart" | "orders" | "profile";
@@ -22,6 +23,30 @@ export default function Header() {
   const [openProfile, setOpenProfile] = useState<boolean>(false);
   const router = useRouter();
   const { logout, isLogin } = useUserStore();
+  const { fetchSuggestions, suggestions, clearSuggestions } = useSearchStore();
+  const [query, setQuery] = useState<string>("");
+  const [searchFocus, setSearchFocus] = useState<boolean>(false);
+  const searchRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      if (query.length > 2) {
+        fetchSuggestions(query);
+      }
+    }, 500);
+    return () => clearTimeout(handler);
+  }, [query]);
+
+  useEffect(() => {
+    if (!searchFocus || query.length < 3) {
+      clearSuggestions();
+    }
+  }, [query, searchFocus]);
+
+  const handleEnter = () => {
+    router.push(`/search?title=${query}`);
+    searchRef.current?.blur();
+  };
 
   const handleNavClick = (route: string) => {
     setOpenMenu(false);
@@ -29,10 +54,12 @@ export default function Header() {
   };
 
   return (
-    <header className="w-full h-20 bg-white shadow-sm lg:px-30 fixed px-10 py-4 flex justify-between items-center top-0 left-0 gap-4 z-50">
+    <header className="w-full h-20 bg-white shadow-sm lg:px-30 fixed px-5 py-4 flex justify-between items-center top-0 left-0 gap-4 z-50">
       {/* Logo */}
       <h1
-        className="flex-shrink-0 text-xl font-bold cursor-pointer"
+        className={`shrink-0 text-xl font-bold cursor-pointer ${
+          searchFocus ? "max-sm:hidden" : "opacity-100"
+        }`}
         onClick={() => router.push("/")}
       >
         🛒 E-Shop
@@ -43,10 +70,40 @@ export default function Header() {
         <Icon icon="mdi:search" className="w-6 h-6" color="#9e9e9e" />
         <input
           type="text"
+          name="Search bar"
           placeholder="Search"
-          className="outline-none w-full bg-transparent"
+          value={query}
+          ref={searchRef}
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+          onFocus={() => setSearchFocus(true)}
+          onBlur={() => setSearchFocus(false)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleEnter();
+            }
+          }}
+          className="outline-none w-full bg-transparent font-medium"
         />
       </div>
+      {/* Search results */}
+      {suggestions.length > 0 && (
+        <div className="absolute top-20  w-full mx-auto left-1/2 transform -translate-x-1/2 bg-white border border-gray-200 shadow-lg rounded-md p-2 max-w-3xl  max-h-96">
+          {suggestions.map((suggestion) => (
+            <button
+              key={suggestion._id}
+              onClick={() => {
+                setSearchFocus(false);
+                router.push(`/product/${suggestion._id}`);
+              }}
+              className="block w-full text-left px-3 py-2 hover:bg-gray-100 rounded-md cursor-pointer"
+            >
+              {suggestion.title}
+            </button>
+          ))}
+        </div>
+      )}
 
       {/* Desktop Nav */}
       <nav className="space-x-4 flex max-sm:hidden items-center relative">
@@ -103,7 +160,7 @@ export default function Header() {
       </nav>
 
       {/* Mobile Menu Toggle */}
-      <div className="sm:hidden">
+      <div className={`sm:hidden ${searchFocus ? "hidden" : "block"}`}>
         <Icon
           onClick={() => setOpenMenu(!openMenu)}
           icon="mdi:menu"
@@ -119,7 +176,10 @@ export default function Header() {
       >
         <div className="flex flex-col gap-4 p-6 h-full overflow-y-auto">
           {/* Close Button */}
-          <button className="self-end mb-4 cursor-pointer" onClick={() => setOpenMenu(false)}>
+          <button
+            className="self-end mb-4 cursor-pointer"
+            onClick={() => setOpenMenu(false)}
+          >
             <Icon icon="mdi:close" className="text-2xl" />
           </button>
 
