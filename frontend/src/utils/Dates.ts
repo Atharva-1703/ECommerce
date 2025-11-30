26f6788de3af60aac190e8a50e88a119b765f503
Atharva-1703 <141930344+Atharva-1703@users.noreply.github.com>
2025-11-30 18:02:49 +0530

    refactor(util/Dates): changed the code function to a Dayjs module  - converted getExpectedDeliveryDate() and getFormattedDate() to use the module  - added getAge() function to get the age upto todays date

--- frontend/src/utils/Dates.ts
+++ frontend/src/utils/Dates.ts
@@ -1,32 +1,26 @@
+import dayjs from "dayjs";
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+dayjs.extend(relativeTime);
+
 export function getExpectedDeliveryDate(): {
   date: string;
   label: string;
 } {
-  const today = new Date();
-
   const randomDays = Math.floor(Math.random() * (7 - 3 + 1)) + 3;
 
-  const deliveryDate = new Date(today);
-  deliveryDate.setDate(today.getDate() + randomDays);
-
-  const options: Intl.DateTimeFormatOptions = {
-    month: "short",
-    day: "numeric",
-  };
-  const formattedDate = deliveryDate.toLocaleDateString(undefined, options);
+  const delivery = dayjs().add(randomDays, "day");
 
   return {
-    date: deliveryDate.toISOString(),
-    label: `Expected delivery by ${formattedDate}`,
+    date: delivery.toISOString(),
+    label: `Expected delivery by ${delivery.format("MMM D")}`,
   };
 }
 
 export const getFormattedDate = (UTCDate: string) => {
-  const date = new Date(UTCDate);
-  const day = date.getDate();
-  const weekDay = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][
-    date.getDay()
-  ];
-  const month = date.toLocaleString("default", { month: "short" });
-  return `${day} ${month}, ${weekDay}`;
+  return dayjs(UTCDate).format("D MMM,ddd");
 };
+
+export const getAge=(UTCDate: string) => {
+  return dayjs(UTCDate).fromNow();
+}
