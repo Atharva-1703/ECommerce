df91c00dc27f26fa07152f6096771bac033231ec
Atharva-1703 <141930344+Atharva-1703@users.noreply.github.com>
2025-12-24 15:13:28 +0530

    style(Profile Overview): modified for small displays

--- frontend/src/components/Profile/ProfileOverview.tsx
+++ frontend/src/components/Profile/ProfileOverview.tsx
@@ -6,14 +6,12 @@ const ProfileOverview = () => {
   return (
     <div className="space-y-6">
       <h2 className=" text-xl font-semibold mb-4">Account Information</h2>
-      <div className="bg-gray-50 rounded-xl p-6 border border-gray-300">
-        <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center mb-4">
-          <div>
+      <div className="bg-gray-50 rounded-xl py-4 px-3 border border-gray-300">
+        <div className="flex flex-col  mb-4">
             <p className="text-gray-500 text-sm">Username</p>
             <p className="text-gray-900 font-medium text-lg">{user?.username}</p>
-          </div>
         </div>
-        <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center">
+        <div className="flex flex-col">
           <div>
             <p className="text-gray-500 text-sm">Email</p>
             <p className="text-gray-900 font-medium text-lg">{user?.email}</p>
