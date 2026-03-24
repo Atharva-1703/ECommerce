438504b89ac5a188813d7651c6d9f583f7820e43
Atharva-1703 <141930344+Atharva-1703@users.noreply.github.com>
2026-03-24 14:16:44 +0530

    fix(sidebar): build fix

--- frontend/src/components/admin/Sidebar.tsx
+++ frontend/src/components/admin/Sidebar.tsx
@@ -9,8 +9,8 @@ export default function SideBar({ tabs , onSelectTab,selectedTab}: SideBarProps)
     <div className="w-64 p-4">
       <h1 className="text-lg font-semibold">Admin Panel</h1>
       <div className="flex flex-col w-[90%] gap-2 mt-3">
-        {tabs.map((tab) => (
-          <div className={`p-3 rounded-lg  cursor-pointer ${selectedTab === tab ? "bg-black text-white" : "hover:bg-gray-200"} `} onClick={()=>onSelectTab(tab)}>
+        {tabs.map((tab,index) => (
+          <div key={index} className={`p-3 rounded-lg  cursor-pointer ${selectedTab === tab ? "bg-black text-white" : "hover:bg-gray-200"} `} onClick={()=>onSelectTab(tab)}>
             {tab}
           </div>
         ))}
